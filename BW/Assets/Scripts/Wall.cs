bd7605dfa84e7e2acc6ddb23daf197b0bd027dcd
RyuKata <ryuichi.pad@gmail.com>
2015-01-05 17:26:20 +0900

    Floor set

---
+++ BW/Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+	public Sprite Black;
+	public Sprite White;
+	SpriteRenderer mainRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			mainRenderer = gameObject.GetComponent<SpriteRenderer> ();
+
+			if (mainRenderer.sprite == Black) {
+				mainRenderer.sprite = White;
+			} else {
+				mainRenderer.sprite = Black;
+			}
+
+			gameObject.collider2D.enabled = !gameObject.collider2D.enabled;
+		}
+	}
+}
