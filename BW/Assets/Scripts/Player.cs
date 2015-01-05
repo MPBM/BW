927b50e1927729f893e150a39d11282eb4be3a82
RyuKata <ryuichi.pad@gmail.com>
2015-01-05 15:57:30 +0900

    Player walk

---
+++ BW/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	Vector3 speed = new Vector3(0.05f,0,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D obj){
+		if (obj.collider.tag == "Wall") {
+			speed = -speed;
+		}
+	}
+}
